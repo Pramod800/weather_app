0a59199281f4faec151dd7ed96e7a28f40f15f1a
Pramod800 <timilsinapramod8888@gmail.com>
2023-05-07 13:11:03 +0545

    models created and home screen created

---
+++ lib/weather/presentation/screens/weather_home_screen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class WeatherHomeScreen extends StatefulWidget {
+  const WeatherHomeScreen({super.key});
+
+  @override
+  State<WeatherHomeScreen> createState() => _WeatherHomeScreenState();
+}
+
+class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
